a1c5a6414af7a9cab43ac9ed019b34273ffffb7e
Akhila640 <akhilasiddi00@gmail.com>
2024-11-08 10:35:40 +0530

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,29 @@
+function b(){
+let k=document.getElementById("p");
+let a=document.getElementById("btn");
+// a.onclick=function b(){
+k.style.backgroundColor="red"
+k.style.fontSize="60px"
+k.style.borderRadius="50%"
+k.style.display="inline"
+console.log(k)
+}
+function c()
+{
+    let images=document.getElementById("div");
+    let button=document.getElementById("button2");
+    button.style.width="100px"
+    button.style.backgroundColor="red"
+    images.innerHTML=
+    `<img id="i1" src="https://static.vecteezy.com/system/resources/thumbnails/028/626/672/small/hd-image-ai-generative-free-photo.jpeg" width="150px">
+    <img  id="i2" src="https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg" width="200px">
+    <img  id="i3" src="https://gratisography.com/wp-content/uploads/2024/03/gratisography-funflower-800x525.jpg" width="200px">`
+
+let image1=document.getElementById("i1")
+image1.style.borderRadius="30px"
+let image2=document.getElementById("i2")
+image2.style.borderRadius="100px"
+let image3=document.getElementById("i3")
+image3.style.borderRadius="50%"
+
+}
